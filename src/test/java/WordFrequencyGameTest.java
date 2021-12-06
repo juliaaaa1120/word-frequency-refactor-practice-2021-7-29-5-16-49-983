c5dd6351da2455df513ba399ae1c3849c67b59b2
Julia Ng <ngju@oocl.com>
2021-12-06 18:11:39 +0800

    [WordFrequencyGame][JuliaNg] - refactor: rename validate_input_string_process_to_expected_result

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -10,7 +10,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     @Test
@@ -18,7 +18,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     @Test
@@ -34,7 +34,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the   \n   is";
         String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     @Test
@@ -42,7 +42,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the the is";
         String expectResult = "the 2\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     @Test
@@ -50,7 +50,7 @@ public class WordFrequencyGameTest {
         //Given
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+        validate_input_string_process_to_expected_result(inputStr, expectResult);
     }
 
     private void validate_input_string_process_to_expected_result(String inputStr, String expectResult) {
